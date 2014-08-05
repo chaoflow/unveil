0f770d0fd00d6fd561eda12ae796d554cb3d1f77
Florian Friesdorf <flo@chaoflow.net>
2014-08-05 14:45:12 +0200

    merge create-meta into meta command and advance finding of dists

--- unveil/__init__.py
+++ unveil/__init__.py
@@ -8,9 +8,10 @@ __version__ = '0.20140726'
 
 
 import click
+import itertools
 import logging
 import os
-import json
+from glob import glob
 
 from .distinfo import Distribution
 
@@ -65,19 +66,53 @@ def create_scripts(ctx, dists, force, python, target):
             os.chmod(abspath, os.stat(abspath).st_mode | UMASKED_EXE)
 
 
-@cli.command(name="create-meta")
-@click.option(
-    '--dist', 'dists',
-    callback=lambda ctx, dists: tuple(Distribution(distinfopath=x) for x in dists),
-    multiple=True,
-    type=click.Path(exists=True),
-    help="Paths to .dist-info directories",
-)
-@click.pass_context
-def create_meta(ctx, dists):
-    log = ctx.log = ctx.parent.log.getChild('create-meta')
+def find_dists(path):
+    if path.endswith('.dist-info'):
+        dists = (path,)
+    elif path.endswith('site-packages'):
+        dists = glob(os.path.join(path, '*.dist-info'))
+    else:
+        dists = glob(os.path.join(path, 'lib', '*', 'site-packages', '*.dist-info'))
+    return tuple(Distribution(distinfopath=x) for x in dists)
 
-    dists_meta = [json.load(open(dist.metafile)) for dist in dists]
+
+@cli.command(help="""
+Unveil metadata of distributions found in space-separated list of
+PATHS.
+
+A PATH may point at a '*.dist-info' directory of one distribution, a
+'site-packages' directory containing multiple distributions or a
+directory being globbed with 'lib/*/site-packages/*.dist-info'.""")
+@click.option(
+    '--print-names',
+    is_flag=True,
+    help='Print names of distributions found')
+@click.option(
+    '--print-nix-wheels-meta',
+    is_flag=True,
+    help='Print content for wheels-meta file for nixpkgs.')
+@click.option(
+    '--sort/--no-sort',
+    help='Sort distributions by name')
+@click.argument(
+    'paths',
+    nargs=-1,
+    required=True,
+    type=click.Path(exists=True))
+def meta(paths, print_names, print_nix_wheels_meta, sort):
+    dists = tuple(itertools.chain(*(find_dists(x) for x in paths)))
+    if sort:
+        dists = sorted(dists, key=lambda x: x.name)
+
+    if print_names:
+        print('\n'.join(x.name for x in dists))
+
+    if print_nix_wheels_meta:
+        print_nix_meta(dists)
+
+
+def print_nix_meta(dists):
+    dists_meta = [dist.metadata for dist in dists]
     dists_names = [meta['name'] for meta in dists_meta]
 
     print('python: self:')
